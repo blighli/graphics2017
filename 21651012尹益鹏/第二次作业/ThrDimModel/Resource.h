54afe62edca49a7a2171d12722ac20b6879c1460
NazgulLee <lijian1994@hotmail.com>
2017-04-12 23:31:24 +0800

    第二次作业

---
+++ 21651012尹益鹏/第二次作业/ThrDimModel/Resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Mol_Simu.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_MOL_SITYPE                  129
+#define ID_translate                    32771
+#define ID_rotate                       32772
+#define ID_scale                        32773
+#define ID_VDWsurface                   32774
+#define ID_PolySurf                     32775
+#define ID_RationalPolySurf             32776
+#define ID_InitialMesh                  32777
+#define ID_p1fSurf                      32778
+#define ID_p2fsurf                      32779
+#define ID_p1p2fSurf                    32780
+#define ID_MENUITEM32781                32781
+#define ID_MENUITEM32782                32782
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
