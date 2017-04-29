d51cb736d703b7fbc788c6160831df98a78fe14a
Armas <awxyb25en8@163.com>
2017-04-29 12:53:39 +0800

    (郭志强)读书报告+第三次作业

---
+++ 21651160郭志强/homework_03/MySolarSystem/Shader.h
@@ -0,0 +1,16 @@
+#ifndef SHADER_H
+#define SHADER_H
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <GL/glew.h>
+
+class Shader {
+public:
+	GLuint Program;
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+	void Use();
+};
+
+#endif   !SHADER_H
