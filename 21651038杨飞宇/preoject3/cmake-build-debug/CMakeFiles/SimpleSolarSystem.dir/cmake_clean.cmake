bbafcb0eef2478cdaee1a9230dbea1d62476a8c2
yangfeiyu20102011 <yangfeiyu20102011@163.com>
2017-04-29 16:35:23 +0800

    Add files via upload

---
+++ 21651038杨飞宇/preoject3/cmake-build-debug/CMakeFiles/SimpleSolarSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleSolarSystem.dir/main.cpp.o"
+  "SimpleSolarSystem.pdb"
+  "SimpleSolarSystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleSolarSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
